c78b6e031a3042fdbc0918364390d8713aaa47b9
GATITO <universo-16@hotmail.com>
2021-07-24 03:31:14 -0500

    primer commit

---
+++ src/PRUEBAS_1.lua
@@ -0,0 +1,9 @@
+nombre="Carlos Amadeus"
+apellidos="Álvarez Rocha"
+edad=21
+futuro_sueldo=500.999
+dispositivos={laptop=2,pc=1,celulares=5,consolas=1}
+lenguajes_de_progra={"c++","python","javascript","kotlin","c#","lua"}
+luz_en_casa=true
+vacio=nil
+
